4b958d8a943b284707cba07b80138eb1c82383f3
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-18 00:22:21 +0900

    feat(ui) Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Footer from './ui/footer/footer.tsx';
+
 function App() {
   return(
   <Router>
     <Routes>
       <Route path='/' />
     </Routes>
+    <Footer />
   </Router>
   )
 }
---
+++ client/src/ui/footer/footer.tsx
@@ -0,0 +1,52 @@
+import { Row, Col } from "antd";
+import { Link } from "react-router-dom";
+import { MailOutlined, FacebookOutlined, TwitterOutlined, InstagramOutlined} from "@ant-design/icons";
+import styles from './footer.module.css';
+
+function Footer() {
+  return (
+    <div>
+      <div className={styles.footerContainer}>
+        <Row gutter={16}>
+          <Col>
+            <Link to="/privacy" className={styles.footerLink}>
+              개인정보처리방침
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/notice" className={styles.footerLink}>
+              공지사항
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/ask" className={styles.footerLink}>
+              문의하기
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/about" className={styles.footerLink}>
+              서비스 설명
+            </Link>
+          </Col>
+          <Col>
+              <MailOutlined /> Business Mail : novelia000@gmail.com
+          </Col>
+          <Col>
+            <div className="footer-text">All rights reserved</div>
+          </Col>
+          <Col>
+            <FacebookOutlined />
+          </Col>
+          <Col>
+            <TwitterOutlined />
+          </Col>
+          <Col>
+            <InstagramOutlined />
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
