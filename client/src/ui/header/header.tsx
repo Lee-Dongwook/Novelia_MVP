62aa28295c2fe7c4d6a9b2ed87e5c112288009a8
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-19 00:35:21 +0900

    fix(header)

--- client/src/ui/header/header.tsx
+++ client/src/ui/header/header.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Row, Col, Input } from 'antd';
-import { BellOutlined, EditOutlined, UserOutlined } from '@ant-design/icons';
 import styles from './header.module.css';
+import headerLogo from '../../image/headerLogo.png';
+import headerNotice from '../../image/headerNotice.png';
+import headerUser from '../../image/headerUser.png';
+import headerWrite from '../../image/headerWrite.png';
 
 function Header() {
   return (
@@ -9,19 +12,19 @@ function Header() {
       <div className={styles.headerContainer}>
         <Row gutter={70}>
           <Col>
-            Noveila
+            <img src={headerLogo} alt='헤더로고' style={{width: '120px'}}/>
           </Col>
           <Col>
             <Input style={{width: 500, textAlign: 'center'}} type='text' placeholder='검색어를 입력하세요'/>
           </Col>
-          <Col>
-            <BellOutlined />
+          <Col> 
+            <img src={headerNotice} alt='헤더알림' style={{width: '25px', height: '25px'}} />
           </Col>
           <Col>
-            <EditOutlined />
+            <img src={headerWrite} alt='헤더작품등록' style={{width: '25px', height: '25px'}} />
           </Col>
           <Col>
-            <UserOutlined />
+            <img src={headerUser} alt='헤더마이페이지' style={{width: '25px', height: '25px'}} />
           </Col>
         </Row>
       </div>
